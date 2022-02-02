213d278de3cf2e3f24cf405567cc5a4cba364c6a
K.Ichikawa <43563179+ichika035@users.noreply.github.com>
2022-02-02 12:03:40 +0900

    Update wakachi.py

--- wakachi.py
+++ wakachi.py
@@ -23,25 +23,3 @@ for text in read_text:
     with open("koamchi_wakati.txt", encoding = "utf_8", mode="a") as write_file:
         write_file.writelines(result)
 
-"""
-FILE_NAME = "komachi.txt"
-
-with open(FILE_NAME, "r", encoding="utf-8") as f:
-    CONTENT = f.read()
-
-tagger = MeCab.Tagger()
-parse = tagger.parse(CONTENT)
-
-
-fi = open(sys.argv[1], 'r')
-fo = open(sys.argv[2], 'w')
-
-line = fi.readline()
-while line:
-    result = tagger.parse(line)
-    fo.write(result[1:]) # skip first \s
-    line = fi.readline()
-
-fi.close()
-fo.close()
-"""
